4c1a8ae25e33af9e2468b9ab77d56bfd3a9f054c
Amodhx <amxdhnanditha@gmail.com>
2025-04-05 14:28:55 +0530

    created custom exceptions

---
+++ src/main/java/com/example/TreineticTaskManager/exceptions/TaskNotFoundException.java
@@ -0,0 +1,15 @@
+package com.example.TreineticTaskManager.exceptions;
+
+public class TaskNotFoundException extends RuntimeException{
+    public TaskNotFoundException() {
+        super();
+    }
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+
+    public TaskNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
