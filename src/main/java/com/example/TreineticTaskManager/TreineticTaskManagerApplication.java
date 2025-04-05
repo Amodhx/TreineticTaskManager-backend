a581cf539a4e05780cdca495e9e0ad9efed4d99d
Amodhx <amxdhnanditha@gmail.com>
2025-04-05 09:52:43 +0530

    Initial commit

---
+++ src/main/java/com/example/TreineticTaskManager/TreineticTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TreineticTaskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreineticTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreineticTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TreineticTaskManager/TreineticTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TreineticTaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreineticTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
