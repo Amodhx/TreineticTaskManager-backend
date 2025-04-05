8ab800fec474d2cad0488b58c1c91f0557bcbb04
Amodhx <amxdhnanditha@gmail.com>
2025-04-05 13:16:01 +0530

    added model mapper to project

---
+++ src/main/java/com/example/TreineticTaskManager/util/MappingObjects.java
@@ -0,0 +1,25 @@
+package com.example.TreineticTaskManager.util;
+
+import com.example.TreineticTaskManager.dto.impl.TaskDTO;
+import com.example.TreineticTaskManager.entity.impl.TaskEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MappingObjects {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public TaskDTO taskEntityToDTO(TaskEntity taskEntity){
+        return modelMapper.map(taskEntity, TaskDTO.class);
+    }
+    public List<TaskDTO> taskEntityListToDTOList(List<TaskEntity> taskEntities) {
+        return taskEntities.stream()
+                .map(this::taskEntityToDTO)
+                .toList();
+    }
+}
